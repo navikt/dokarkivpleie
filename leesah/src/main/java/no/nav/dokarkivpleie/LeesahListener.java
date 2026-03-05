b7a12b14491771eab9aa21f1ab1e6b1f1d7d0905
Anders Opskar Voldsund <anders.voldsund@nav.no>
2026-03-05 09:07:20 +0100

    MMA-8564 Logg partisjon og offset på melding

--- leesah/src/main/java/no/nav/dokarkivpleie/LeesahListener.java
+++ leesah/src/main/java/no/nav/dokarkivpleie/LeesahListener.java
@@ -3,11 +3,14 @@ package no.nav.dokarkivpleie;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.person.pdl.leesah.Personhendelse;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static no.nav.dokarkivpleie.DoedsfallhendelseMapper.mapPersonhendelseTilDoedsfall;
 import static no.nav.dokarkivpleie.Karp001Service.OPPLYSNINGSTYPE_DOEDSFALL;
+import static org.springframework.kafka.support.KafkaHeaders.OFFSET;
+import static org.springframework.kafka.support.KafkaHeaders.RECEIVED_PARTITION;
 
 @Slf4j
 @Service
@@ -24,8 +27,13 @@ public class LeesahListener {
 			topics = "pdl.leesah-v1",
 			groupId = "dokarkivpleie"
 	)
-	public void lesPersonhendelserFraLeesah(Personhendelse personhendelse) {
+	public void lesPersonhendelserFraLeesah(
+			Personhendelse personhendelse,
+			@Header(RECEIVED_PARTITION) int partition,
+			@Header(OFFSET) long offset
+	) {
 		if (OPPLYSNINGSTYPE_DOEDSFALL.equals(personhendelse.getOpplysningstype().toString())) {
+			log.info("Mottok melding fra partition {} med offset {}", partition, offset);
 			karp001Service.behandleDoedsfallhendelseFraLeesah(mapPersonhendelseTilDoedsfall(personhendelse));
 		}
 	}
