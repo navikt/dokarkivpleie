f1532534b8419d0ea5ec50bcccec3487b23096c3
Joakim Borgersen <joakim.borgersen@nav.no>
2026-04-22 13:42:11 +0200

    Flytt logginslag ut av for-loop (#24)
    * Flytt logginslag ut av for-loop
    
    * Logg talet på personar vi har behandla arkivsaker for
    
    * Køyr skass001 kl. 7 måndag til fredag
    
    ---------
    
    Co-authored-by: Anders Opskar Voldsund <anders.voldsund@nav.no>

--- skass001/src/main/java/no/nav/dokarkivpleie/MerkSakerBevaringstidPassertScheduler.java
+++ skass001/src/main/java/no/nav/dokarkivpleie/MerkSakerBevaringstidPassertScheduler.java
@@ -27,7 +27,7 @@ public class MerkSakerBevaringstidPassertScheduler {
 		this.slackService = slackService;
 	}
 
-	@Scheduled(initialDelay = 60000L)
+	@Scheduled(cron = "0 0 7 * * MON-FRI")
 	public void kjoerPeriodiskJobb() {
 		log.info("Starter Skass001 for å markere saker der bevaringstid har passert for tema={}.", TEMA_MED_STOETTEDE_BEVARINGSTIDER);
 
